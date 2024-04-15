36acd9b1e35bb309225489055847279ce7e4a3d2
TK-Pihon <164522702+TK-Pihon@users.noreply.github.com>
2024-04-15 18:06:40 +0900

    refactor: "push 에러 수정"
    일단 급하게 다른분들의 PR이 작동하도록 main의 요소를 수정했습니다

--- Hello_Korea/settings.py
+++ Hello_Korea/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'GyeongJu',
+    'GyeongJu.apps.GyeongjuConfig',
 ]
 
 MIDDLEWARE = [
