848690ae21a907c9507ac9ace45ba878033ef529
out_of_anjoong <joongh0113@gmail.com>
2024-04-15 19:31:43 +0900

    feat: 전통체험 정보 추가

--- GyeongJu/urls.py
+++ GyeongJu/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from .Accomodation import accomodation_views
+from .KoreaTradition import tradition_views
 
 urlpatterns = [
     # localhost:8000/GyeongJu
     path('', views.index, name='index'),
     path('Accomodation', accomodation_views.accomodation_view),
+    path('Tradition', tradition_views.tradition_view),
 ]
