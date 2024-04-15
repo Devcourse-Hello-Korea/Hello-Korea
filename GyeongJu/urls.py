1baf089c3a6eed09be494d16c725cd1fde236371
out_of_anjoong <joongh0113@gmail.com>
2024-04-15 19:44:02 +0900

    fix: urls.py 수정

--- GyeongJu/urls.py
+++ GyeongJu/urls.py
@@ -7,9 +7,6 @@ urlpatterns = [
     # localhost:8000/GyeongJu
     path('', views.index, name='index'),
     path('Accomodation', accomodation_views.accomodation_view),
-<<<<<<< HEAD
     path('Tradition', tradition_views.tradition_view),
-=======
     path('food', views.food_index),
->>>>>>> main
 ]
