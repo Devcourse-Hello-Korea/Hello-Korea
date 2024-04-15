5f0732def54e4f7b49f051cc58323bcbc1f1cbf3
TK-Pihon <164522702+TK-Pihon@users.noreply.github.com>
2024-04-15 15:09:40 +0900

    feat: "Accomodation 추가
    Accomodation 폴더를 만들어 추가했습니다.
    템플릿 폴더와 템플릿폴더 적용을 위한 세팅내에 "GyeongJu"를 추가했습니다
    urls에 Accomodation 경로를 추가했습니다

---
+++ GyeongJu/Accomodation/accomodation_crawl.py
@@ -0,0 +1,83 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from .accomodation_models import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+def set_dates_and_crawl(url, checkin_date, checkout_date):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+
+    driver.get(url)
+
+    date_click = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[1]/div[1]/c-wiz/div/div/div[1]/div/div[1]/div[2]/div/div/div[2]/div[1]/div/input')
+    date_click.click()
+
+    checkin_input = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[1]/div[1]/c-wiz/div/div/div[1]/div/div[1]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/input')
+    checkin_input.click()
+    checkin_input.send_keys(Keys.CONTROL, 'a')
+    checkin_input.send_keys(Keys.BACK_SPACE)
+    checkin_input.send_keys(checkin_date)
+    checkin_input.send_keys(Keys.ENTER)
+
+    checkout_input = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[1]/div[1]/c-wiz/div/div/div[1]/div/div[1]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div/input')
+    checkout_input.click()
+    checkout_input.send_keys(Keys.CONTROL, 'a')
+    checkout_input.send_keys(Keys.BACK_SPACE)
+    checkout_input.send_keys(checkout_date)
+    checkout_input.send_keys(Keys.ENTER)
+
+    start_element = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[1]/div[1]/c-wiz/div/div/div[1]/div/div[1]/div[2]/div/div[2]/div/div[2]/div[4]/div/button[2]/span')
+    start_element.click()
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[1]/div[2]/div[2]/main/c-wiz/span/c-wiz/c-wiz[4]/div/a')))   
+    first_element = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[1]/div[2]/div[2]/main/c-wiz/span/c-wiz/c-wiz[4]/div/a')
+    first_element.click()
+
+    # 탭이 포함된 상위 div 선택
+    tabs_container_xpath = '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[2]/div[1]/div/div/c-wiz/div/div[2]/div/div[2]'
+    wait = WebDriverWait(driver, 10)
+    tabs_container = wait.until(EC.presence_of_element_located((By.XPATH, tabs_container_xpath)))
+
+    # 탭의 총 개수 파악
+    tabs = tabs_container.find_elements(By.XPATH, './div')
+    num_of_tabs = len(tabs)
+
+    #for i in range(1, num_of_tabs + 1):
+    for i in range(3, 10 + 3):
+        # 탭 클릭
+        tab_xpath = f'{tabs_container_xpath}/div[{i}]'
+        wait = WebDriverWait(driver, 10)
+        tab = wait.until(EC.visibility_of_element_located((By.XPATH, tab_xpath)))
+        driver.execute_script("arguments[0].click();", tab)
+
+        try:
+            accomodation_name = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[2]/div[2]/div[1]/div[1]/c-wiz/div/h1'))).text
+        except NoSuchElementException:
+            accomodation_name = ""  # 호텔 이름이 없는 경우
+
+        try:
+            accomodation_price = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[2]/div[2]/div[2]/div[2]/c-wiz/div/div/div[2]/span[1]/c-wiz[1]/c-wiz[1]/div/section/div[1]/div[2]/c-wiz/div[1]/div/span/button/span/span[2]').text
+        except NoSuchElementException:
+            try:
+                accomodation_price = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[2]/div[2]/div[2]/div[2]/c-wiz/div/div/div[2]/span[1]/c-wiz[1]/c-wiz[1]/div/section/div[1]/div[2]/c-wiz/div[1]/div/span/button/span/div[1]/span[3]/span').text
+            except NoSuchElementException:
+                accomodation_price = "0"  # 호텔 가격이 없는 경우
+        try:
+            accomodation_location = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/c-wiz/div[1]/div[2]/div[2]/div[2]/div[2]/c-wiz/div/div/div[2]/span[1]/c-wiz[1]/c-wiz[1]/div/section/div[1]/div[1]/div/div[2]/span').text
+        except NoSuchElementException:
+            accomodation_location = ""  # 호텔 위치가 없는 경우
+
+        accomodation_price_int = convert_price_to_int(accomodation_price)
+        # Django 모델에 저장
+        accomodation_info = AccomodationInfo(location=accomodation_location, name=accomodation_name, price=accomodation_price_int)
+        accomodation_info.save()
+
+    driver.quit()
+
+def convert_price_to_int(price_str):
+    numeric_str = price_str.replace('₩', '').replace(',', '')
+    return int(numeric_str)
---
+++ GyeongJu/Accomodation/accomodation_views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .accomodation_models import AccomodationInfo
+from .accomodation_forms import CheckinCheckoutForm
+from .accomodation_crawl import set_dates_and_crawl
+
+def accomodation_view(request):
+    data = None
+    if request.method == 'POST':
+        form = CheckinCheckoutForm(request.POST)
+        if form.is_valid():
+            checkin_date = form.cleaned_data['checkin_date']
+            checkin_date_str = "{}월 {}".format(checkin_date.month, checkin_date.day)
+            checkout_date = form.cleaned_data['checkout_date']
+            checkout_date_str = "{}월 {}".format(checkout_date.month, checkout_date.day)
+            AccomodationInfo.objects.all().delete()
+            set_dates_and_crawl("https://www.google.com/travel/search?q=%EA%B2%BD%EC%A3%BC%20%EC%88%99%EC%86%8C&g2lb=2503771%2C2503781%2C4284970%2C4291517%2C4814050%2C4874190%2C4893075%2C4965990%2C72277293%2C72302247%2C72317059%2C72406588%2C72414906%2C72421566%2C72458066%2C72462234%2C72470440%2C72470899%2C72471280%2C72472051%2C72473841%2C72481458%2C72485656%2C72485658%2C72486593%2C72494250%2C72513422%2C72513513%2C72523972%2C72530239%2C72534000%2C72536387%2C72538597%2C72549171%2C72549174%2C72561417%2C72561423&hl=ko-KR&gl=kr&ssta=1&ts=CAESCgoCCAMKAggDEAAaHhIcEhQKBwjoDxAEGA8SBwjoDxAEGBAYATIECAAQACoHCgU6A0tSVw&qs=CAAgACgA&ap=KigKEglWdogmq6tBQBF8kGVhiiRgQBISCXPseYl8GUJAEXyQZSH_OmBAMABoAQ&ictx=111&ved=0CAAQ5JsGahcKEwjAxZq6k7-FAxUAAAAAHQAAAAAQdw", 
+                                checkin_date_str, checkout_date_str)
+            data = AccomodationInfo.objects.all()  
+        else:
+            data = None
+    else:
+        form = CheckinCheckoutForm()
+        data = None
+    return render(request, 'GyeongJu/accomodation_index.html', {'form': form, 'data': data})
