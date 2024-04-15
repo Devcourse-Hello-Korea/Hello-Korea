0fdf80804b0c9960b8bb8984781f1b7f3e171466
TK-Pihon <164522702+TK-Pihon@users.noreply.github.com>
2024-04-15 17:27:25 +0900

    refactor: "crawl 코드 분할"
    crawl 코드를 3개의 def로 분할하였습니다.
    date_inputs, process_accomodation_tab, set_dates_and_crawl
    이제 이 3개의 함수가 기존의 함수를 대체합니다.

--- GyeongJu/Accomodation/accomodation_models.py
+++ GyeongJu/Accomodation/accomodation_models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class AccomodationInfo(models.Model):
-    location = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
     name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
     class Meta:
