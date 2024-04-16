005c90ccd11f4947212bd0be6f98a03bfc104eba
Elisha0510 <sjeong35@knu.ac.kr>
2024-04-16 14:45:01 +0900

    refactor: 로딩 시간 단축

--- GyeongJu/food/food_data.py
+++ GyeongJu/food/food_data.py
@@ -35,7 +35,7 @@ def get_food_data(url, kind, page):
                 chinese_model = chinese_food_model(name=shop_name, address=shop_address, number=shop_number)
                 chinese_model.save()
 
-        time.sleep(1)
+        #time.sleep(1)
 
 
 
