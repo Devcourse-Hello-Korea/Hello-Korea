afb36050a62026efc0584c3e1a257cea20adaf36
TK-Pihon <164522702+TK-Pihon@users.noreply.github.com>
2024-04-16 15:50:31 +0900

    remove: "example_J 삭제"

--- Example_J/Project1/p_site/p_datas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PDatasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'p_datas'
--- Example_J/Project1/p_site/p_datas/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class CheckinCheckoutForm(forms.Form):
-    checkin_date = forms.DateField(label='체크인 날짜', widget=forms.TextInput(attrs={'type': 'date'}))
-    checkout_date = forms.DateField(label='체크아웃 날짜', widget=forms.TextInput(attrs={'type': 'date'}))
