356a6ed9a52cdcc70d151c59a49dbcded3deb16c
Elisha0510 <sjeong35@knu.ac.kr>
2024-04-16 12:58:04 +0900

    feat: 한식 페이지 경로 추가

--- GyeongJu/urls.py
+++ GyeongJu/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from .Accomodation import accomodation_views
+from .food import food_views
 
 urlpatterns = [
     # localhost:8000/GyeongJu
     path('', views.index, name='index'),
     path('Accomodation', accomodation_views.accomodation_view),
-    path('food', views.food_index),
+    path('food', food_views.food_index),
+    path('food/korean', food_views.korean_food_view),
 ]
