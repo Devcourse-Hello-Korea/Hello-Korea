f34848dd47b869e6901a046ffc72bd8688c4d113
TK-Pihon <164522702+TK-Pihon@users.noreply.github.com>
2024-04-16 15:44:08 +0900

    feat: "accomodation 크롤링 개선"
    검색과 크롤링 기능을 분할하였습니다.
    이미지도 가져올 수 있게 만들었습니다.
    하이퍼링크도 가져올 수 있게 만들었습니다.
    다양한 종류의 에러를 대처할 수 있게 만들었습니다.

--- GyeongJu/Accomodation/accomodation_models.py
+++ GyeongJu/Accomodation/accomodation_models.py
@@ -3,6 +3,11 @@ from django.db import models
 class AccomodationInfo(models.Model):
     location = models.CharField(max_length=200)
     name = models.CharField(max_length=100)
-    price = models.IntegerField(default=0)
+    month = models.CharField(max_length=4)
+    link = models.URLField(max_length=300)
+    img_src1 = models.TextField(null=True)
+    img_src2 = models.TextField(null=True)
+    img_src3 = models.TextField(null=True)
     class Meta:
         app_label = 'GyeongJu'
+
