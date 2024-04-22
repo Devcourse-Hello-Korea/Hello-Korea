3af1dad05b5116091307d5f5dc1915f5817f81d7
TK-Pihon <164522702+TK-Pihon@users.noreply.github.com>
2024-04-22 17:47:53 +0900

    fix: "페이지들에 요소 삽입"
    번역용 텍스트를 변수로 삽입하였습니다

--- GyeongJu/views.py
+++ GyeongJu/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 def index(request):
-    return render(request, 'Home.html')
+    lang = request.COOKIES.get('user_lang', 'ko')
+    text_data=hard_coded_text.objects.filter(lang = lang).first()
+    return render(request, 'Home.html', {'hard_coded': text_data})
 
 def gyeongju_index(request):
-    return render(request, 'GyeongJu_Home.html')
+    lang = request.COOKIES.get('user_lang', 'ko')
+    text_data=hard_coded_text.objects.filter(lang = lang).first()
+    return render(request, 'GyeongJu_Home.html', {'hard_coded': text_data})
 
 def sooncheon_index(request):
-    return render(request, 'Sooncheon_Home.html')
+    lang = request.COOKIES.get('user_lang', 'ko')
+    text_data=hard_coded_text.objects.filter(lang = lang).first()
+    return render(request, 'Sooncheon_Home.html', {'hard_coded': text_data})
 
 def jeonju_index(request):
-    return render(request, 'Jeonju_Home.html')
+    lang = request.COOKIES.get('user_lang', 'ko')
+    text_data=hard_coded_text.objects.filter(lang = lang).first()
+    return render(request, 'Jeonju_Home.html', {'hard_coded': text_data})
